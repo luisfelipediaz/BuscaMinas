9df0d2ed123757ed45b080dc9985b9842e97d9a6
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-04-08 12:09:54 -0500

    Docker configuration

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:9.11.1
+WORKDIR /usr/src/app
+
+RUN npm install -g @angular/cli@next && npm cache clean --force
+
+RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y chromium
+
+ENV CHROME_BIN=chromium
+
+EXPOSE 4200
+EXPOSE 9876
+EXPOSE 49152
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -15,6 +15,12 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
@@ -28,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false
   });
 };
