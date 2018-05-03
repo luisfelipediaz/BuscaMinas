6c6c0cd105345f7a59ce87bf3ab7050807bebdb3
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-05-02 22:35:05 -0500

    Avances

--- src/app/minesweeper/minesweeper.spec.ts
+++ src/app/minesweeper/minesweeper.spec.ts
@@ -1,13 +1,22 @@
-import { Minesweeper } from './minesweeper';
+import { Minesweeper, Cell, generateMatrixWithNewsCells } from './minesweeper';
+
+describe('GenerateMatrixWithNewsCells', () => {
+    it('should "generateMatrixWithNewsCells" return a new matrix expected', () => {
+        const cell: Cell = {
+            isMine: false,
+            beaten: false,
+            probability: 0
+        };
+        const expected = new Array(10).fill(new Array(9).fill(cell));
+        const actual = generateMatrixWithNewsCells({ rows: 10, columns: 9 });
+
+        expect(actual).toEqual(expected);
+    });
+});
 
 describe('Minesweeper', () => {
-    let minesweeper: Minesweeper;
-
-    beforeEach(() => {
-        minesweeper = new Minesweeper(8, 8, 10);
-    });
-
     it('should create by correct dimensions', () => {
+        const minesweeper = new Minesweeper(8, 8, 10);
         expect(minesweeper).toBeTruthy();
     });
 
@@ -18,8 +27,9 @@ describe('Minesweeper', () => {
     });
 
     it('should "getMatrix" return correct value', () => {
-        const expected = new Array(8).fill(new Array(8));
-        const actual = minesweeper.getMatrix();
+        const gameFake = { matrix: generateMatrixWithNewsCells({ rows: 8, columns: 8 }) };
+        const expected = generateMatrixWithNewsCells({ rows: 8, columns: 8 });
+        const actual = Minesweeper.prototype.getMatrix.call(gameFake);
 
         expect(actual).toEqual(expected);
     });
@@ -31,5 +41,55 @@ describe('Minesweeper', () => {
         expect(Minesweeper.prototype.createMines).toHaveBeenCalledWith(12);
     });
 
-    it('should createMines call 8 times "createMine"');
+    it('should createMines call 8 times "generateNewMine"', () => {
+        spyOn(Minesweeper.prototype, 'generateNewMine');
+
+        Minesweeper.prototype.createMines.call(Minesweeper.prototype, 8);
+
+        expect(Minesweeper.prototype.generateNewMine).toHaveBeenCalledTimes(8);
+    });
+
+    it('should "generateNewMine" create two random and in this position generate new mine', () => {
+        const gameFake = {
+            matrix: generateMatrixWithNewsCells({ rows: 8, columns: 9 }),
+            dimensions: { rows: 8, columns: 9 }
+        };
+        const expectedCell: Cell = {
+            beaten: false,
+            isMine: true,
+            probability: 0
+        };
+        spyOn(Math, 'random').and.returnValue(0.5);
+        spyOn(Math, 'floor').and.returnValue(4);
+
+        Minesweeper.prototype.generateNewMine.call(gameFake);
+
+        expect(Math.random).toHaveBeenCalledTimes(2);
+        expect(Math.floor).toHaveBeenCalledTimes(2);
+        expect(gameFake.matrix[4][4]).toEqual(expectedCell);
+    });
+
+    fit('should "generateNewMine" if selected cell its mine, retry until find cell to not a mine', () => {
+        debugger;
+        let countEntry = 0;
+        const gameFake = {
+            matrix: generateMatrixWithNewsCells({ rows: 8, columns: 9 }),
+            dimensions: { rows: 8, columns: 9 }
+        };
+        gameFake.matrix[4][4].isMine = true;
+
+        const expectedCell: Cell = {
+            beaten: false,
+            isMine: true,
+            probability: 0
+        };
+
+        spyOn(Math, 'random').and.returnValue(0.5);
+        spyOn(Math, 'floor').and.callFake(() => { if (countEntry < 2) { countEntry++; return 4; } return 3; });
+
+        Minesweeper.prototype.generateNewMine.call(gameFake);
+        expect(Math.random).toHaveBeenCalledTimes(4);
+        expect(Math.floor).toHaveBeenCalledTimes(4);
+        expect(gameFake.matrix[3][3]).toEqual(expectedCell);
+    });
 });
--- src/app/minesweeper/minesweeper.ts
+++ src/app/minesweeper/minesweeper.ts
@@ -1,8 +1,10 @@
 export class Minesweeper {
     private matrix: Cell[][];
+    private dimensions: { rows: number, columns: number };
 
     constructor(rows: number, columns: number, private countMines: number) {
-        this.matrix = new Array(rows).fill(new Array(columns));
+        this.matrix = new Array(rows).fill(new Array(columns).fill({ ...newCell }));
+        this.dimensions = { rows, columns };
         this.createMines(countMines);
     }
 
@@ -11,13 +13,39 @@ export class Minesweeper {
     }
 
     createMines(countMines: number) {
+        for (let index = 0; index < countMines; index++) {
+            this.generateNewMine();
+        }
+    }
 
+    generateNewMine() {
+        let row: number;
+        let column: number;
+
+        do {
+            row = Math.floor(Math.random() * this.dimensions.rows);
+            column = Math.floor(Math.random() * this.dimensions.columns);
+        } while (this.matrix[row][column].isMine);
+
+        this.matrix[row][column].isMine = true;
     }
 
     getMatrix(): any[][] {
         return this.matrix;
     }
 }
+
+export function generateMatrixWithNewsCells(dimensions: { rows: number, columns: number }): Cell[][] {
+    const matrix: Cell[][] = new Array(dimensions.rows);
+    matrix.fill({...new Array(dimensions.columns).fill({ ...newCell })});
+    return matrix;
+}
+
+const newCell: Cell = {
+    isMine: false,
+    beaten: false,
+    probability: 0
+};
 
 export interface Cell {
     isMine: boolean;
